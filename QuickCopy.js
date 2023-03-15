2d449b74e6c4eb41a05b58866ee3377a66ee1f73
Nate Bream <nathanielbream@gmail.com>
2023-03-15 19:41:45 -0400

    Create QuickCopy.js

---
+++ QuickCopy.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         Quick Copy
+// @namespace    Torn/Recruitment
+// @version      0.3
+// @downloadURL
+// @description  Copy all usernames and links from Advanced Search
+// @author       MisterCow
+// @match        *://www.torn.com/page.php?sid=UserList*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+// Variables that will probably change
+var buttonName = "COPY PROFILES";
+var genericProfileLink = "www.torn.com/profiles.php?XID=";
+var hiddenTag = "tt-hidden";
+
+
+// Add a button
+var zNode = document.createElement('div');
+zNode.innerHTML = '<button id="myButton" type="button">' + buttonName + '</button>';
+zNode.setAttribute('id', 'myContainer');
+document.body.appendChild(zNode);
+
+// Activate the newly added button.
+document.getElementById("myButton").addEventListener ("click", ButtonClickAction, false);
+
+function ButtonClickAction (zEvent) {
+    var x = document.querySelectorAll("a");
+    var copyString = "";
+
+    for (var i = 0; i < x.length; i++)
+    {
+        var cleanText = x[i].textContent.replace(/\s+/g, ' ').trim();
+        var cleanLink = x[i].href;
+
+        if(cleanLink.includes(genericProfileLink))
+        {
+            var parent = x[i].parentElement.parentElement.classList.value;
+            if (parent.includes("user") && !parent.includes(hiddenTag))
+            {
+                // Format for proper pasting into excel/sheets
+                copyString += '=HYPERLINK("' + cleanLink + '","' + cleanText + '")\n'
+            }
+        }
+    };
+
+    try
+    {
+        setTimeout(async()=>console.log(await navigator.clipboard.writeText(copyString)), 250);
+    }
+    catch (err)
+    {
+        console.error('Failed to copy: ', err);
+    }
+}
