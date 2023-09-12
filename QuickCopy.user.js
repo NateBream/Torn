918ca3bfa54a0e19b2303cdd6e0cc3c5e9c8ccaf
Nate Bream <nathanielbream@gmail.com>
2023-09-11 21:22:29 -0400

    Update QuickCopy.user.js

--- QuickCopy.user.js
+++ QuickCopy.user.js
@@ -4,7 +4,7 @@
 // @version      0.3
 // @downloadURL  https://raw.githubusercontent.com/NateBream/Torn/main/QuickCopy.user.js
 // @description  Copy all usernames and links from Advanced Search
-// @author       MisterCow
+// @author       MisterCow [2879457] & Echotte [2834135]
 // @match        *://www.torn.com/page.php?sid=UserList*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @grant        none
@@ -15,7 +15,7 @@ var buttonName = "COPY PROFILES";
 var genericProfileLink = "www.torn.com/profiles.php?XID=";
 var hiddenTag = "tt-hidden";
 
-
+/* PREVIOUS CODE - NOT USED NOW
 // Add a button
 var zNode = document.createElement('div');
 zNode.innerHTML = '<button id="myButton" type="button">' + buttonName + '</button>';
@@ -24,6 +24,24 @@ document.body.appendChild(zNode);
 
 // Activate the newly added button.
 document.getElementById("myButton").addEventListener ("click", ButtonClickAction, false);
+*/
+
+// NEW CODE
+setTimeout(func, 1000);
+
+//Adds a button to trigger the calls.
+function func() {
+    'use strict';
+    let addBtn = document.querySelector("div.users-list-title.title-black.top-round.m-top10.clearfix"); //Adds the button BEFORE the title bar
+    let btn = document.createElement('BUTTON');
+    btn.id = "employ";
+    btn.innerHTML = ' COPY NAMES';
+    btn.type = "button";
+    btn.className = 'torn-btn';
+    addBtn.insertAdjacentElement('beforebegin', btn);
+    document.getElementById("employ").addEventListener("click", ButtonClickAction);
+};
+// END NEW CODE
 
 function ButtonClickAction (zEvent) {
     var x = document.querySelectorAll("a");
